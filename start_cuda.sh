2dc34f1d9e9958f902d4f81d545787d7bd737c7a
nodenew <158447089+nodenew@users.noreply.github.com>
2024-02-01 12:01:57 +0400

    Add files via upload

---
+++ start_cuda.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+npm install
+
+if [ -z "$1" ]
+then
+  echo "Please specify the number of GPUs as an argument."
+  exit 1
+fi
+
+while true; do
+  for i in $(seq 0 $(($1 - 1)))
+  do
+    node send_universal.js --api lite --bin ./pow-miner-cuda --gpu $i &
+  done
+  sleep 1
+  wait
+done
---
+++ start_opencl.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+npm install
+
+if [ -z "$1" ]
+then
+  echo "Please specify the number of GPUs as an argument."
+  exit 1
+fi
+
+while true; do
+  for i in $(seq 0 $(($1 - 1)))
+  do
+    node send_universal.js --api lite --bin ./pow-miner-opencl --gpu $i &
+  done
+  sleep 1
+  wait
+done
